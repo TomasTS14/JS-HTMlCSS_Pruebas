5cf8162de8e7da639ea37c712255339472142e35
Tomas Casa WIn <210099@fppiramide.com>
2022-05-29 01:11:30 +0200

    probando funciones

--- tickerOverlord/js/movimientoPagina.js
+++ tickerOverlord/js/movimientoPagina.js
@@ -1,1 +1,20 @@
 
+// document.getElementById("Bienvenido").onclick(function () {
+//     document.getElementB yId("Bienvenido").style.backgroundColor = 'black';
+// });
+flag = false;
+
+function cambiarTituloColor() {
+
+    const titulo = document.getElementsByTagName("h1")[0];
+    if (flag == false) {
+        flag = true;
+        titulo.onclick = function () {
+            titulo.style.color = "red";
+        }
+    } else if (flag == true) {
+        titulo.onclick = function () {
+            titulo.style.color = "white";
+        }
+    }
+};
