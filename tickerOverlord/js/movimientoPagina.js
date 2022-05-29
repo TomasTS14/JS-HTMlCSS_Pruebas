b014bd60adfbfcaa54795ecac046c1c0a986e6b4
Tomas Casa WIn <210099@fppiramide.com>
2022-05-29 22:30:12 +0200

    animates title and section div on scroll

--- tickerOverlord/js/movimientoPagina.js
+++ tickerOverlord/js/movimientoPagina.js
@@ -2,19 +2,43 @@
 // document.getElementById("Bienvenido").onclick(function () {
 //     document.getElementB yId("Bienvenido").style.backgroundColor = 'black';
 // });
-flag = false;
 
-function cambiarTituloColor() {
+function changeColor(element) {
 
-    const titulo = document.getElementsByTagName("h1")[0];
-    if (flag == false) {
-        flag = true;
-        titulo.onclick = function () {
-            titulo.style.color = "red";
-        }
-    } else if (flag == true) {
-        titulo.onclick = function () {
-            titulo.style.color = "white";
+    element.style.color = 'red';
+
+}
+
+// const cambioTitulo = () => {
+//     flag = false;
+//     document.getElementsByTagName("h1")[0].addEventListener('mouseover', () => {
+//         if (flag == false) {
+//             document.getElementsByTagName("h1")[0].style.color = 'red';
+//             flag = true;
+//         } else {
+//             document.getElementsByTagName("h1")[0].style.color = 'white';
+//             flag = false;
+//         }
+
+//     })
+// }
+
+
+
+const divResizedByScroll = () => {
+    let divMovedList = document.getElementsByClassName("divInternoBase");
+    for (let i = 0; i < divMovedList.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = divMovedList[i].getBoundingClientRect().top;
+        let elementVisible = 100;
+        if (elementTop < windowHeight - elementVisible) {
+            divMovedList[i].classList.add("active");
+        } else {
+            divMoved[i].classList.remove("active");
         }
     }
-};
+}
+
+window.addEventListener('scroll', divResizedByScroll);
+
+
